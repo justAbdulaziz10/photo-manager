9da1b61da42c4011ba74eb9edc55d6ca395d2a58
abdulaziz <140005102+justAbdulaziz10@users.noreply.github.com>
2025-03-09 16:54:35 +0300

    Update Photo.java

--- S2CSC212Project/src/Photo.java
+++ S2CSC212Project/src/Photo.java
@@ -1,15 +1,17 @@
 
 public class Photo {
+	String path;
 
 	// Constructor
 	public Photo(String path, LinkedList<String> tags) {
+		this.path=path;
 		
 	}
 	
 	
-	// Return the full file name (the path) of the photo. A photo is uniquely identified byits path.
+	// Return the full file name (the path) of the photo. A photo is uniquely identified by its path.
 	public String getPath() {
-		
+		return path;
 	}
 
 	
