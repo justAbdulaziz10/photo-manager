acf29740bc039f50c443080c64130d49d60a1836
abdulaziz <140005102+justAbdulaziz10@users.noreply.github.com>
2025-03-19 11:54:22 +0300

    Update Album.java

--- S2CSC212Project/src/Album.java
+++ S2CSC212Project/src/Album.java
@@ -1,8 +1,9 @@
-
-public class Album {
+ public class Album {
 	String name;
 	String condition;
 	PhotoManager manager;
+	private int tagComparisons = 0;
+
 	
 	
 
@@ -10,7 +11,7 @@ public class Album {
 	public Album(String name, String condition, PhotoManager manager) {
 		this.name=name;
 		this.condition=condition;
-		
+		this.manager=manager;
 	}
 	
 	
@@ -33,21 +34,171 @@ public class Album {
 	
 	
 	// Return all photos that satisfy the album condition
-	public LinkedList<Photo> getPhotos(){
-		return null; //we need to make it
+	public LinkedList<Photo> getPhotos(){	
+		
+	    if (condition == null || condition.isEmpty() || manager == null) {
+	        return new LinkedList<>();
+	    }
+	    
+	    if (condition.contains("AND")) {
+	        String[] conditions = condition.split("AND");
+	        for (int i = 0; i < conditions.length; i++) {
+	            conditions[i] = conditions[i].trim();
+	        }
+	        
+	        LinkedList<Photo> matchingPhotos = getTagPhoto(conditions[0]);
+	        
+	        for (int i = 1; i < conditions.length; i++) {
+	            LinkedList<Photo> nextConditionPhotos = getTagPhoto(conditions[i]);
+	            matchingPhotos = AND(matchingPhotos, nextConditionPhotos);
+	        }
+	        
+	        return matchingPhotos;
+	    } else {
+	        return getTagPhoto(condition);
+	    }
+	}
+
+	
+	public boolean tagInPhoto(String tagToFind, LinkedList<String> tagList) {
+		if(tagToFind == null || tagList==null) {
+			return false;
+		}
+		
+		tagList.findFirst();
+		while(true) {
+			tagComparisons++;
+			if(tagToFind.equals(tagList.retrieve())) {
+				return true;
+			}
+			
+			if(tagList.last()) {
+				break;
+			}
+			
+			tagList.findNext();
+		}
+		
+		return false;
 		
 	}
-	//The list of photos that belong to the album is determined at the time when the method
-	//getPhotos in Album is called, not when the album is created.
 	
+	public boolean photoExist(LinkedList<Photo> L, Photo P) {
+		
+		if (L == null || P == null) {
+	        return false;
+	    }
+	    
+	    String pathToFind = P.getPath();
+	    
+	    L.findFirst();
+	    while (true) {
+	    	Photo currentPhoto = L.retrieve();
+	        
+	        if (currentPhoto != null && pathToFind.equals(currentPhoto.getPath())) {
+	            return true;
+	        }
+	       
+	        if (L.last()) {
+	            break;
+	        }
+	        
+	        L.findNext();
+	    }
+	    
+	    return false;
+		
+	}
 	
-	// Return the number of tag comparisons used to find all photos of the album
-	//The method public int getNbComps(): Computes the number of tag comparisons
-	//made to determine the photos belonging to an album. Its purpose is to track efficiency
-	//by counting tag comparisons
+	LinkedList<Photo> getTagPhoto(String tag){
+		LinkedList<Photo> result = new LinkedList<Photo>();
+	    
+	    if (tag == null || this.getManager() == null) {
+	        return result;
+	    }
+	    
+	    LinkedList<Photo> allPhotos = this.getManager().getPhotos();
+	    
+	    if (allPhotos == null || allPhotos.empty()) {
+	        return result;
+	    }
+	    
+	    allPhotos.findFirst();
+	    while (true) {
+	        Photo currentPhoto = allPhotos.retrieve();
+	        
+	        if (currentPhoto != null) {
+	            LinkedList<String> photoTags = currentPhoto.getTags();
+	            
+	            if (photoTags != null && !photoTags.empty()) {
+	                if (tagInPhoto(tag, photoTags)) {
+	                    result.insert(currentPhoto);
+	                }
+	            }
+	        }
+	        
+	        if (allPhotos.last()) {
+	            break;
+	        }
+	        
+	        allPhotos.findNext();
+	    }
+	    
+	    return result;
+		
+	}
+	
+	LinkedList<Photo> AND(LinkedList<Photo> A, LinkedList<Photo> B){
+		LinkedList<Photo> results = new LinkedList<>();
+	    
+	    if (A == null || A.empty()) {
+	        if (B != null && !B.empty()) {
+	            B.findFirst();
+	            while (true) {
+	                results.insert(B.retrieve());
+	                if (B.last()) {
+	                    break;
+	                }
+	                B.findNext();
+	            }
+	            return results;
+	        } else {
+	            return results;
+	        }
+	    } else if (B == null || B.empty()) {
+	        A.findFirst();
+	        while (true) {
+	            results.insert(A.retrieve());
+	            if (A.last()) {
+	                break;
+	            }
+	            A.findNext();
+	        }
+	        return results;
+	    }
+	    
+	    A.findFirst();
+	    while (true) {
+	        Photo photoA = A.retrieve();
+	        
+	        if (photoExist(B, photoA)) {
+	            results.insert(photoA);
+	        }
+	        
+	        if (A.last()) {
+	            break;
+	        }
+	        A.findNext();
+	    }
+	    
+	    return results;
+	}
+	
 	public int getNbComps() {
-		return 0; // we need to make it
-		
+		tagComparisons = 0;
+	    getPhotos();  
+	    return tagComparisons;
 	}
+
 	
 }
