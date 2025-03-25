7e6ae9a8f1fbc1f3568579b0e24fc7d009353e63
abdulaziz <140005102+justAbdulaziz10@users.noreply.github.com>
2025-03-25 13:58:21 +0300

    Update BooleanWrapper.java

--- S2CSC212Project/src/BooleanWrapper.java
+++ S2CSC212Project/src/BooleanWrapper.java
@@ -1,15 +1,1 @@
-class BooleanWrapper {
-    boolean value;
 
-    public BooleanWrapper(boolean value) {
-        this.value = value;
-    }
-
-    public void set(boolean value) {
-        this.value = value;
-    }
-
-    public boolean get() {
-        return value;
-    }
-}
