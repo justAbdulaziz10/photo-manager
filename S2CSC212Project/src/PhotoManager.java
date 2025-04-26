fe8c93cbd426b39294b2e11715bdd0ea0e9ae70f
abdulaziz <140005102+justAbdulaziz10@users.noreply.github.com>
2025-04-26 04:08:34 +0300

    Update PhotoManager.java

--- S2CSC212Project/src/PhotoManager.java
+++ S2CSC212Project/src/PhotoManager.java
@@ -1,34 +1,238 @@
 public class PhotoManager {
-    private LinkedList<Photo> photos;
- // Constructor
+	private LinkedList<Photo> photos;
+	private BST<LinkedList<Photo>> invertedIndex; 
+	private BST<String> tagNameMap; 
+	
     public PhotoManager() {
         photos = new LinkedList<>();
+        invertedIndex = new BST<>();
+        tagNameMap = new BST<>();
     }
- // Return all managed photos
+    
     public LinkedList<Photo> getPhotos() {
         return photos;
     }
- // Add a photo
+    
     public void addPhoto(Photo p) {
+    	if (photoExists(photos, p)) return;
         photos.insert(p);
-    }
- // Delete a photo
-  public void deletePhoto(String path) {
-    if (photos.empty()) {
-        return;
-    }
-    photos.findFirst();
-    if (photos.retrieve().getPath().equals(path)) {
-        photos.remove();
-        return;
-    }
-    while (!photos.last()) {
-        photos.findNext();
-        if (photos.retrieve().getPath().equals(path)) {
-            photos.remove();
-            return;
+
+        LinkedList<String> tags = p.getTags();
+        if (tags.empty()) return;
+
+        tags.findFirst();
+        while (true) {
+            String tag = tags.retrieve();
+            int tagKey = stringToKey(tag);
+            tagNameMap.insert(tagKey, tag);
+
+            if (!invertedIndex.findkey(tagKey)) {
+                LinkedList<Photo> list = new LinkedList<>();
+                list.insert(p);
+                invertedIndex.insert(tagKey, list);
+            } else {
+                invertedIndex.retrieve().insert(p);
+            }
+
+            if (tags.last()) break;
+            tags.findNext();
         }
     }
-}
+    
+    public boolean deletePhoto(String path) {
+        if (photos.empty()) return false;
+
+        photos.findFirst();
+        while (true) {
+            if (photos.retrieve().getPath().equals(path)) {
+                Photo toDelete = photos.retrieve();
+                photos.remove();
+                removeFromIndex(toDelete);
+                return true;
+            }
+            if (photos.last()) break;
+            photos.findNext();
+        }
+
+        return false;
+    }
+    
+    private void removeFromIndex(Photo photo) {
+        LinkedList<String> tags = photo.getTags();
+        if (tags.empty()) return;
+
+        tags.findFirst();
+        while (true) {
+            String tag = tags.retrieve();
+            int tagKey = stringToKey(tag);
+
+            if (invertedIndex.findkey(tagKey)) {
+                LinkedList<Photo> list = invertedIndex.retrieve();
+                removePhotoFromList(list, photo.getPath());
+
+                if (list.empty()) {
+                    invertedIndex.remove_key(tagKey);
+                }
+            }
+
+            if (tags.last()) break;
+            tags.findNext();
+        }
+    }
+    
+    public BST<LinkedList<Photo>> getInvertedIndex() {
+        return invertedIndex;
+    }
+    
+    public String getTagName(int key) {
+        if (tagNameMap.findkey(key)) {
+            return tagNameMap.retrieve();
+        }
+        return "TAG_" + key;
+    }
+    
+    
+    public LinkedList<Photo> getPhotosWithTag(String tag) {
+        LinkedList<Photo> result = new LinkedList<>();
+        int tagKey = stringToKey(tag);
+        if (invertedIndex.findkey(tagKey)) {
+            LinkedList<Photo> found = invertedIndex.retrieve();
+            found.findFirst();
+            while (true) {
+                result.insert(found.retrieve());
+                if (found.last()) break;
+                found.findNext();
+            }
+        }
+        return result;
+    }
+    
+    public String findTagName(int tagKey) {
+        return tagNameMap.findkey(tagKey) ? tagNameMap.retrieve() : ("UNKNOWN_" + tagKey);
+    }
+
+    private int stringToKey(String tag) {
+        int key = 0;
+        for (int i = 0; i < tag.length(); i++) {
+            key = key * 31 + tag.charAt(i);
+        }
+        return key;
+    }
+    
+    private boolean removePhotoFromList(LinkedList<Photo> list, String path) {
+        if (list.empty()) return false;
+        
+        list.findFirst();
+        boolean found = false;
+        
+        while (true) {
+            Photo current = list.retrieve();
+            if (current.getPath().equals(path)) {
+                list.remove();
+                found = true;
+                break;  
+            }
+            
+            if (list.last()) break;
+            list.findNext();
+        }
+        
+        return found;
+    }
+
+    private boolean photoExists(LinkedList<Photo> list, Photo p) {
+        if (list.empty()) return false;
+        list.findFirst();
+        while (true) {
+            if (list.retrieve().getPath().equals(p.getPath())) return true;
+            if (list.last()) break;
+            list.findNext();
+        }
+        return false;
+    }
+
+
+    
+    @Override
+    public String toString() {
+        LinkedList<Photo> photos = getPhotos();
+        int count = 0;
+        if (!photos.empty()) {
+            photos.findFirst();
+            while (true) {
+                count++;
+                if (photos.last()) break;
+                photos.findNext();
+            }
+        }
+        return "PhotoManager with " + count + " photos";
+    }
+    
+    public int getPhotosByTagWithCount(String tag, boolean useInvertedIndex, LinkedList<Photo> output) {
+        int comparisons = 0;
+
+        if (tag == null) return comparisons;
+
+        if (useInvertedIndex) {
+            int tagKey = stringToKey(tag);
+            BSTNode<LinkedList<Photo>> p = invertedIndex.root;
+            
+            while (p != null) {
+                comparisons++;
+                if (tagKey == p.key) {
+                    LinkedList<Photo> photoList = p.data;
+                    photoList.findFirst();
+                    while (true) {
+                        output.insert(photoList.retrieve());
+                        if (photoList.last()) break;
+                        photoList.findNext();
+                    }
+                    break;
+                } else if (tagKey < p.key) {
+                    p = p.left;  
+                } else {
+                    p = p.right;   
+                }}
+            
+        } else {
+            photos.findFirst();
+            while (true) {
+                Photo p = photos.retrieve();
+                LinkedList<String> tags = p.getTags();
+                tags.findFirst();
+                while (true) {
+                    comparisons++;
+                    if (tag.equals(tags.retrieve())) {
+                        output.insert(p);
+                        break;
+                    }
+                    if (tags.last()) break;
+                    tags.findNext();
+                }
+                if (photos.last()) break;
+                photos.findNext();
+            }
+        }
+
+        return comparisons;
+    }
+     
+    public int getBSTComparisonsForTag(String tag) {
+        int tagKey = stringToKey(tag);
+        int count = 0;
+        BSTNode<LinkedList<Photo>> p = invertedIndex.root;
+        while (p != null) {
+            count++;
+            if (tagKey == p.key)
+                break;
+            else if (tagKey < p.key)
+                p = p.left;
+            else
+                p = p.right;
+        }
+        return count;
+    }
+
+
 
 }
