2950225d9f900780c88e18b98103f8f053d88e36
abdulaziz <140005102+justAbdulaziz10@users.noreply.github.com>
2025-05-02 17:51:10 +0300

    Update List.java

--- S2CSC212Project/src/List.java
+++ S2CSC212Project/src/List.java
@@ -2,7 +2,6 @@ public interface List<T>{
  public void findFirst( );
  public void findNext( );
  public T retrieve( );
- public void update(T e);
  public void insert(T e);
  public void remove( );
  public boolean full( );
